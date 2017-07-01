ba9fe6241efe5e4544917331bdf33c6d8f651d1e
rhapsodyai <rhapsody.ai@gmail.com>
2017-06-30 23:17:23 -0700

    updated chart

--- index.js
+++ index.js
@@ -9,36 +9,56 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //added
-
+var nluemots;
+var nluurl;
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-  'username': '{username}',
-  'password': '{password}',
+  'username': '',
+  'password': '',
   'version_date': '2017-02-27'
 });
 
-//added
+var parameters = {
+  'url': 'https://www.google.com',
+  'features': {
+    'entities': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2
+    },
+    'keywords': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2
+    }
+  }
+}
 
 var emots;
 var url = '';
-var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('yourapikeyhere');
+// var AlchemyAPI = require('alchemy-api');
+// var alchemy = new AlchemyAPI('yourapikeyhere');
 
 
 app.get('/', function(req, res) {
+    console.log(1);
     //render index.ejs file
     console.log('get')
     res.render('index.ejs', {val: emots});
+
 });
 
 app.post('/', function(req, res) {
+    console.log(2);
     console.log('post')
     url = req.body.name
+    parameters.url = url;
     analyze();
     console.log(req.body.name);
 });
 
 app.get('/ajax', function(req, res) {
+    console.log(3);
     console.log('ajax')
     //console.log(req)
     //console.log(emots)
@@ -51,15 +71,27 @@ app.listen(3000, function() {
 })
 
 function analyze() {
-    //emotion analysis
-    alchemy.emotions(url, {}, function(err, response) {
-      if (err) throw err;
-      // See http://www.alchemyapi.com/api/html-api-1 for format of returned object
-      var emotions = response.docEmotions;
-
-      // Do something with data
-      console.log(emotions)
+  console.log(4);
+  natural_language_understanding.analyze(parameters, function(err, response) {
+    if (err) {
+      console.log('error:', err);
+    }
+    else {
+      emotions = JSON.stringify(response, null, 2);
+      console.log(emotions);
       emots = emotions;
-    });
+    }
+  });
+
+    //emotion analysis
+    // alchemy.emotions(url, {}, function(err, response) {
+    //   if (err) throw err;
+    //   // See http://www.alchemyapi.com/api/html-api-1 for format of returned object
+    //   var emotions = response.docEmotions;
+    //
+    //   // Do something with data
+    //   console.log(emotions)
+    //   emots = emotions;
+    // });
 
 }
